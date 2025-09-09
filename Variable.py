bff4c591c53aecb73bd73bb9a902fb7506674d30
BrysonPreece <>
2025-09-09 17:55:16 -0400

    Create Variable.py

---
+++ Variable.py
@@ -0,0 +1,20 @@
+x = 5
+y = 10 
+print(x)
+print(y)
+print (x + y)
+print (x - y)
+print (x * y)
+print (x / y)
+print (x**2)
+
+sum = x + y
+squaredx = x**2
+print(sum)
+print(squaredx)
+
+name = "Meguru Bachira"
+print(name)
+
+print(f"My name is {name}!")
+print(f"The sum of X and Y is {sum}!")
