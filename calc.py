524da1d3607996d6d051a553a00cb771dc7f2950
ferusutajp <prestonmetil@gmail.com>
2025-09-12 13:06:09 -0400

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,29 @@
+# preston >
+#calc = short for calculator
+add1 = input("Enter your first addition number! ")
+add2 = input("Enter your second addition number! " )
+add1 = int(add1)
+add2 = int(add2)
+sum = add1 + add2
+print(f"The sum of {add1} and {add2} is {sum}!")
+
+sub1 = input("Enter your first subtraction number! " )
+sub2 = input("Enter your second subtraction number! " )
+sub1 = int(sub1)
+sub2 = int(sub2)
+difference = sub1 - sub2
+print(f"The difference of {sub1} and {sub2} is {difference}!")
+
+mult1 = input("Enter your first multiplication number! " )
+mult2 = input("Enter your second multiplication number! " )
+mult1 = int(mult1)
+mult2 = int(mult2)
+product = mult1 * mult2
+print(f"The product of {mult1} and {mult2} is {product}!")
+
+div1 = input("Enter your first division number! " )
+div2 = input("Enter your second division number! " )
+div1 = int(div1)
+div2 = int(div2)
+quotient = div1 / div2
+print(f"The quotient of {div1} and {div2} is {quotient}")
