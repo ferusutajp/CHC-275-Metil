c3f3c361597e071f7520976135201e60fe295f58
BrysonPreece <PreeceB27@chcstudent.com>
2025-09-15 11:44:09 -0400

    Create branch.py

---
+++ branch.py
@@ -0,0 +1,20 @@
+# preston >
+# calc = short for calculator
+# i like the green comments more
+"""
+green > orange
+"""
+# :p
+"""
+Modulus (mod): %
+a % b = a mod b = remainder after a/b
+a % 2 = 0
+"""
+
+x = 10
+if x % 2 == 0:
+    print(f"{x} is divisible by 2.")
+
+elif x % 3 == 0:
+    print(f"{x} is divisible by 3.")
+    
