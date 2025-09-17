499f1624dee41a2481bd65694c4db36c5bb321e7
BrysonPreece <PreeceB27@chcstudent.com>
2025-09-17 13:58:37 -0400

    Update Looping.py

--- Level 2 Bandit/Looping.py
+++ Level 2 Bandit/Looping.py
@@ -1,3 +1,5 @@
+# preston >
+
 """
 Loop Types: (Easy to hard...)
 -While Loops
@@ -21,3 +23,18 @@ Will constantly check for exit condition
 x = 1
 while x <= 10:
     print(x)
+    x = x + 1
+
+nums = [1,2,3,4,5,6,7,8,9,10]
+
+check = False
+
+while check == False:
+    print("Option One")
+    print("Option Two")
+    print("Option Three")
+    option = input("Select your option. ")
+    if option == "Quit":
+        check = True
+    elif option == "One":
+        print("noob")
