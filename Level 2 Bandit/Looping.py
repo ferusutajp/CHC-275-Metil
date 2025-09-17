ae8a775d372f99cc5d62ad60f66e00346746f47d
BrysonPreece <PreeceB27@chcstudent.com>
2025-09-17 13:37:47 -0400

    Update Looping.py

---
+++ Level 2 Bandit/Looping.py
@@ -0,0 +1,23 @@
+"""
+Loop Types: (Easy to hard...)
+-While Loops
+-For Loops
+-Recursion 
+"""
+
+"""
+Syntax:
+while <exit condition>
+    <repeated action>
+    
+Exit Condition is a boolean
+Will constantly check for exit condition
+    Compete action
+    Check for condition
+    If condition not met
+    Repeat
+"""
+
+x = 1
+while x <= 10:
+    print(x)
