269d9b82e01a8d7cb9e04fc4d7ff6e416bc137b1
BrysonPreece <PreeceB27@chcstudent.com>
2025-09-24 14:28:25 -0400

    we did it 🙏

--- Assignments/hailstone.py
+++ Assignments/hailstone.py
@@ -5,15 +5,11 @@ x = input("Enter your number. ")
 x = int(x)
 
 # Math Process
-"""
-while x > 1 or x < 1:
-    while x % 2:
-        x = x / 2
-        print(x)
-    else:
-        x = 3 * x + 1
-        print(x)
-"""
-
 while x != 1:
-    
+    print(x)
+    if x % 2 == 0:
+        x = x / 2
+    elif x % 2 == 1:
+        x = 3 * x + 1
+if x == 1:
+    print(x)
