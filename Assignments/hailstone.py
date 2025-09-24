ae498d633b3420d81445f0be8d3ebdf3f45d172e
BrysonPreece <PreeceB27@chcstudent.com>
2025-09-24 14:30:06 -0400

    Update hailstone.py

--- Assignments/hailstone.py
+++ Assignments/hailstone.py
@@ -6,19 +6,11 @@ x = int(x)
 
 # Math Process
 while x != 1:
-<<<<<<< HEAD
     print(x)
     if x % 2 == 0:
         x = x / 2
     elif x % 2 == 1:
         x = 3 * x + 1
+
 if x == 1:
     print(x)
-=======
-    while x % 2:
-        x = x / 2
-        print(x)
-    else:
-        x = x + 1
-        print(x)
->>>>>>> aa144e3bb329b5f52d25280fefcdfce320f4c397
