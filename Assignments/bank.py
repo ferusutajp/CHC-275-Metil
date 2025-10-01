2cf146b1a683b4ede0106f25f303ef2d5883adab
BrysonPreece <PreeceB27@chcstudent.com>
2025-10-01 11:44:31 -0400

    Update bank.py

--- Assignments/bank.py
+++ Assignments/bank.py
@@ -1,2 +1,21 @@
 # preston >
+# Bank.py -- Banking System
 
+exitcondition = False
+
+print("Welcome system administrator!")
+while exitcondition == False:
+    print("Which system do you want to access?")
+    print("1. Deposit")
+    print("2. Withdraw")
+    print("3. Transfer")
+    print("4. Add Account")
+    print("5. Remove Account")
+    print("6. List Balances")
+    print("7. Quit")
+    System = input("System accessed... ")
+    
+    # Quit Function
+    if System == "Quit":
+        print("Thank you for using our services!")
+        exitcondition = True
