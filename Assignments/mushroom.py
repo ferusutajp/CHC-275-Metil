33a5ffd01d93a2b350f50b92252fea286cf5e753
BrysonPreece <PreeceB27@chcstudent.com>
2025-10-21 12:29:55 -0400

    Update mushroom.py

--- Assignments/mushroom.py
+++ Assignments/mushroom.py
@@ -1,2 +1,9 @@
 # preston >
 
+sizelist = []
+
+while x != "Checked":
+    check = input("Continue or Quit? ")
+    if check == "Quit":
+        x = "Checked"
+    size = input("What is the size of the mushroom? ")
