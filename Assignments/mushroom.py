5052ee7e1d138c47309108e2db4ce44c002bba45
ferusutajp <prestonmetil@gmail.com>
2025-10-22 11:58:43 -0400

    Update mushroom.py

--- Assignments/mushroom.py
+++ Assignments/mushroom.py
@@ -10,11 +10,16 @@ check = False
 while check == False:
     msize = input("What is the size of the mushroom? (Or Quit) ")
     if msize.isnumeric():
+        msize = int(msize)
         sizelist.append(msize)
         if msize < 100:
-            
+            smallsizelist.append(msize)
+        elif msize >= 100 and msize <= 200:
+            medsizelist.append(msize)
+        elif msize > 200:
+            largesizelist.append(msize)
     elif msize == "Quit".lower():
-        print(smallsizelist)
-        print(medsizelist)
-        print(largesizelist)
+        print(f"Small sizes: {smallsizelist}")
+        print(f"Medium sizes: {medsizelist}")
+        print(f"Large sizes: {largesizelist}")
         check = True
