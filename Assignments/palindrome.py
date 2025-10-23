1a7cd47873f07e73d193282d0e5b6ed383e471e4
ferusutajp <prestonmetil@gmail.com>
2025-10-23 10:58:31 -0400

    Update palindrome.py

--- Assignments/palindrome.py
+++ Assignments/palindrome.py
@@ -1,3 +1,14 @@
 # preston >
 # teague >
 
+word = input("Enter your word: ")
+palidrome = False
+print(f"Your word is: {word}")
+print("Checking to see if your word is a palidrome!")
+
+#
+
+if palidrome == True:
+    print("Your word is a palidrome!")
+elif palidrome == False:
+    print("Your word is not a palidrome!")
