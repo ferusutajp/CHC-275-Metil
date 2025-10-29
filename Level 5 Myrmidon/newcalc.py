baf3174c249a35e5eaadf36e8428139ad12c050a
ferusutajp <prestonmetil@gmail.com>
2025-10-29 12:48:51 -0400

    Update newcalc.py

--- Level 5 Myrmidon/newcalc.py
+++ Level 5 Myrmidon/newcalc.py
@@ -8,14 +8,43 @@ while check == True:
     print("3. Multiplication")
     print("4. Division")
     print("5. Quit")
-    Function = str(input("Which function do you want to access?"))
-    if Function.lower() == "Addition":
+    Function = str(input("Which function do you want to access? "))
+    if Function.lower() == "addition":
         try:
-            x = int(input("Enter first number: "))
-            y = int(input("Enter second number: "))
-            ResultAdd = x + y
+            xa = int(input("Enter first number: "))
+            ya = int(input("Enter second number: "))
+            ResultAdd = xa + ya
             print(ResultAdd)
-        
-    elif Function.lower() == "Subtraction":
-    elif Function.lower() == "Multiplication":
-    elif Function.lower() == "Division":
+        except ValueError:
+            print("ValueError -- Please input numeric values")
+    elif Function.lower() == "subtraction":
+        try:
+            xs = int(input("Enter first number: "))
+            ys = int(input("Enter second number: "))
+            ResultSub = xs - ys
+            print(ResultSub)
+        except ValueError:
+            print("ValueError -- Please input numeric values")
+    elif Function.lower() == "multiplication":
+        try:
+            xm = int(input("Enter first number: "))
+            ym = int(input("Enter second number: "))
+            ResultMul = xm * ym
+            print(ResultMul)
+        except ValueError:
+            print("ValueError -- Please input numeric values")
+    elif Function.lower() == "division":
+        try:
+            xd = int(input("Enter first number: "))
+            yd = int(input("Enter second number: "))
+            ResultDiv = xd / yd
+            print(ResultDiv)
+        except ValueError:
+            print("ValueError -- Please input numeric values")
+        except ZeroDivisionError:
+            print("ZeroDivisionError -- Cannot divide by zero")
+    elif Function.lower() == "quit":
+        check = False
+        print("Calculator closed")
+    else:
+        print("Invalid input -- Please select a valid function or quit")
