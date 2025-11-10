9661c9f7a1070ad68407c2e8b80fc0a110dc3873
ferusutajp <prestonmetil@gmail.com>
2025-11-10 12:02:53 -0500

    Update marketevo.py

--- Assignments/marketevo.py
+++ Assignments/marketevo.py
@@ -8,9 +8,11 @@ day2 = open("day21_40.txt", "r")
 buffer2 = day2.readlines()
 day2.close()
 
-msft = (buffer1[0]).strip()
-amzn = (buffer1[1]).strip()
-nvda = (buffer1[2]).strip()
+msft = (buffer1[0]).strip().split(",").pop(0)
 
-msft2 = (buffer2[0]).strip()
-amzn2 = (buffer2[1]).strip()
+amzn = (buffer1[1]).strip().split(",").pop(0)
+nvda = (buffer1[2]).strip().split(",").pop(0)
+
+msft2 = (buffer2[0]).strip().split(",")
+amzn2 = (buffer2[1]).strip().split(",")
+nvda = (buffer2[2]).strip().split(",")
