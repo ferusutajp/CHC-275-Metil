42095101bf8759af668a1505582d3bdeb99ba656
BrysonPreece <PreeceB27@chcstudent.com>
2025-11-10 11:45:52 -0500

    a

--- Assignments/marketevo.py
+++ Assignments/marketevo.py
@@ -3,8 +3,14 @@
 day1 = open("day1_20.txt", "r")
 buffer1 = day1.readlines()
 day1.close()
-print(buffer1)
 
-msft = (buffer1[0])
-amzn = (buffer1[1])
-nvda = (buffer1[2])
+day2 = open("day21_40.txt", "r")
+buffer2 = day2.readlines()
+day2.close()
+
+msft = (buffer1[0]).strip()
+amzn = (buffer1[1]).strip()
+nvda = (buffer1[2]).strip()
+
+msft2 = (buffer2[0]).strip()
+amzn2 = (buffer2[1]).strip()
